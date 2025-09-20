54bc1899bfd28baea101c18b482136977ae681e6
HeWei <1514229897@qq.com>
2025-09-20 18:57:25 +0800

    aaa
    111

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(gradleLibs.plugins.android.application) apply false
-    alias(gradleLibs.plugins.android.library) apply false
-    alias(gradleLibs.plugins.kotlin.android) apply false
-    alias(gradleLibs.plugins.kotlin.jvm) apply false
-    alias(gradleLibs.plugins.kotlin.serialization) apply false
-    alias(gradleLibs.plugins.versions)
-}
